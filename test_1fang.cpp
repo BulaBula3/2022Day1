c5b264760cce87973c7678c3cf7402d982cde70e
fang <98085603+BulaBula3@users.noreply.github.com>
2022-01-20 20:14:06 +0800

    Add files via upload

---
+++ test_1fang.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+#define Month 12
+int main()
+{
+    cout << "一年总共有：" <<Month<<"个月"<< endl;
+    
+    const int Day = 7;
+        cout << "一周总共有：" << Day << "天" << endl;
+    system("pause");
+
+    return 0;
+}
---
+++ test_2fang.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main2()
+{
+    int num1 = 10;
+    int num2 = 40;
+    int sum = num1 + num2;
+    cout << sum << endl;
+
+    system("pause");
+
+    return 0;
+}
